2f75756359358af9bf3bae631d3249b20f35cc74
sanckh <corey.sutton7@gmail.com>
2024-12-12 18:54:55 -0600

    track page view analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { SubscriptionProvider } from "./contexts/SubscriptionContext";
 import Index from "./pages/Index";
@@ -13,9 +13,24 @@ import Templates from "./pages/Templates";
 import History from "./pages/History";
 import Success from "./pages/Success";
 import SubscriptionConfirm from "./pages/SubscriptionConfirm";
-import Settings from "./pages/Settings"; // Added import statement
+import Settings from "./pages/Settings";
+import { trackPageView } from './utils/analytics';
+import { useEffect } from 'react';
 
 const queryClient = new QueryClient();
+
+const RouteChangeTracker = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    trackPageView(
+      document.title,
+      location.pathname
+    );
+  }, [location]);
+
+  return null;
+};
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -25,6 +40,7 @@ const App = () => (
           <Toaster />
           <Sonner />
           <BrowserRouter>
+            <RouteChangeTracker />
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/builder" element={<Builder />} />
@@ -34,7 +50,7 @@ const App = () => (
               <Route path="/history" element={<History />} />
               <Route path="/success" element={<Success />} />
               <Route path="/subscription-confirm" element={<SubscriptionConfirm />} />
-              <Route path="/settings" element={<Settings />} /> // Added new route
+              <Route path="/settings" element={<Settings />} />
             </Routes>
           </BrowserRouter>
         </TooltipProvider>
