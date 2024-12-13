8f7d2077ab4c76ad8c37b6ccb2155ef81b2450da
sanckh <corey.sutton7@gmail.com>
2024-12-13 17:53:32 -0600

    hosting stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,25 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  build: {
+    outDir: mode === 'development' ? 'dist/dev' : 'dist/prod',
+    emptyOutDir: true,
+  },
+  envDir: '.',
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  optimizeDeps: {
+    exclude: ['server/*'],
   },
 }));
